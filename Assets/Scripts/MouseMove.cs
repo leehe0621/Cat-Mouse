73ea5dac3f42e699841adfa3c6467efc7fdff996
leehe0621 <leehe0621@naver.com>
2022-09-27 16:44:32 +0900

    Revert "science room update"
    This reverts commit eda01b469eee13d0eec76a6f185a3d76a997e816.

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -14,8 +14,7 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
-    public Portal portal;
-    int jumpCount = 2;
+    int jumpCount = 3;
 
     void Awake()
     {
